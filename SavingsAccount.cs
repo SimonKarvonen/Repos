7877222e628b8dc9ff2d2bba7aa844451101ebf0
SimonKarvonen <71773525+SimonKarvonen@users.noreply.github.com>
2020-10-20 13:01:29 +0200

    Add files via upload

---
+++ SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; private set; }
+
+        public SavingsAccount(double interestRate, string accountOwner, string accountNumber)
+            : base(accountOwner, accountNumber)
+        {
+            InterestRate = interestRate;
+        }
+
+        public double CalculateInterestRate()
+        {
+            //Amount är protected, så vi kommer åt den här eftersom SavingsAccount ärver från Account
+            Amount = Amount * InterestRate;
+
+            return Amount;
+        }
+
+        //Bad idea..... Här kan vi gömma en hel del jobbiga buggar
+        //public double Deposit(double amount)
+        //{
+        //    return 0.0;
+        //}
+    }
+}
